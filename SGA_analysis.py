a08ab6c290a2c05fade9774cf6518197d86510c1
Matheus Carvalho <mdocarm2@uwo.ca>
2023-10-26 10:42:24 -0400

    Update SGA_analysis.py

--- SGA_analysis.py
+++ SGA_analysis.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from astropy.table import Table
 
 # Read the data from the FITS file
-file = Table.read('/home/mdocarm/Downloads/SGA-2020.fits')
+file = Table.read('C:/Users/mathe/Downloads/SGA-2020.fits')
 df = file.to_pandas()
 df = df[df.D25_LEDA>0.25]
 
@@ -21,7 +21,7 @@ lower_10_percent = int(total_points * 0.9)
 lower_bin_edges = bins[:-1][np.where(np.cumsum(hist) <= lower_10_percent)[0][-1]]
 
 # Select data points in the lower 10%
-selected_data = df[df['SB_D25_LEDA'] >= lower_bin_edges]
+low_sb = df[df['SB_D25_LEDA'] >= lower_bin_edges]
 
 plt.hist(df['SB_D25_LEDA'], bins=1000)
 plt.axvline(x=lower_bin_edges, color='red', linestyle='--', label='10% Cutoff')
@@ -30,6 +30,18 @@ plt.xlabel('Mean Surface Brightness (B band, $mag \; arcsec^{-2}$)')
 plt.legend()
 plt.clf()
 
-print(selected_data['SB_D25_LEDA'])
-print(selected_data['SB_D25_LEDA'].min())
+# Calculate the mean and standard deviation
+mean_sb = low_sb['SB_D25_LEDA'].mean()
+std_sb = low_sb['SB_D25_LEDA'].std()
+mean_d = low_sb['D25_LEDA'].mean()
+std_d = low_sb['D25_LEDA'].std()
 
+sb_cutoff = mean_sb + 2*std_sb
+d_cutoff = mean_d + 2*std_d
+
+low_sb = low_sb[low_sb['SB_D25_LEDA'] < sb_cutoff]
+low_sb = low_sb[low_sb['D25_LEDA'] < d_cutoff]
+
+
+plt.scatter(low_sb['SB_D25_LEDA'], low_sb['D25_LEDA'])
+plt.show()
