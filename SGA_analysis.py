c7e4a217085a174dde7e17410153b79bd82279fc
Matheus Carvalho <113134149+mattcarv@users.noreply.github.com>
2023-10-27 15:44:42 -0400

    Update SGA_analysis.py

--- SGA_analysis.py
+++ SGA_analysis.py
@@ -4,23 +4,26 @@ import matplotlib.pyplot as plt
 from astropy.table import Table
 
 # Read the data from the FITS file
-file = Table.read('C:/Users/mathe/Downloads/SGA-2020.fits')
-df = file.to_pandas()
+df = pd.read_csv('/home/mdocarm/Downloads/SGA_xmatch.csv')
+df = df[['SGA_ID_1', 'GALAXY', 'RA_LEDA', 'DEC_LEDA', 'MORPHTYPE', 'PA_LEDA',
+         'D25_LEDA', 'BA_LEDA', 'Z_LEDA', 'SB_D25_LEDA', 'FLUX_G', 'FLUX_R',
+         'FLUX_Z', 'FLUX_W1', 'FLUX_W2', 'FLUX_W3', 'FLUX_W4']]
+
 df = df[df.D25_LEDA>0.25]
 
 # Create a histogram
 hist, bins = np.histogram(df['SB_D25_LEDA'], bins=1000)
 
-# Calculate the total number of data points
+# Get the total number of data points
 total_points = len(df['SB_D25_LEDA'])
 
-# Calculate the number of data points in the lower 10%
+# Calculate the number of data points in the lower 10% magnitudes
 lower_10_percent = int(total_points * 0.9)
 
 # Find the bin edges that correspond to the lower 10% of data
 lower_bin_edges = bins[:-1][np.where(np.cumsum(hist) <= lower_10_percent)[0][-1]]
 
-# Select data points in the lower 10%
+# Select data points in the lower 10% magnitudes
 low_sb = df[df['SB_D25_LEDA'] >= lower_bin_edges]
 
 plt.hist(df['SB_D25_LEDA'], bins=1000)
@@ -30,7 +33,7 @@ plt.xlabel('Mean Surface Brightness (B band, $mag \; arcsec^{-2}$)')
 plt.legend()
 plt.clf()
 
-# Calculate the mean and standard deviation
+# Calculate the mean and standard deviation for each
 mean_sb = low_sb['SB_D25_LEDA'].mean()
 std_sb = low_sb['SB_D25_LEDA'].std()
 mean_d = low_sb['D25_LEDA'].mean()
@@ -42,9 +45,9 @@ d_cutoff = mean_d + 2*std_d
 low_sb = low_sb[low_sb['SB_D25_LEDA'] < sb_cutoff]
 low_sb = low_sb[low_sb['D25_LEDA'] < d_cutoff]
 
-print(low_sb)
+# print(low_sb)
 
 plt.scatter(low_sb['SB_D25_LEDA'], low_sb['D25_LEDA'], alpha=0.3)
 plt.ylabel('Major axis diameter at the $25 \; mag \; arcsec^{-2}$ isophote (arcmin)')
 plt.xlabel('Mean Surface Brightness (B band, $mag \; arcsec^{-2}$)')
-plt.clf()
+plt.show()
