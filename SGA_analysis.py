fc7c996b3bf16415ae4d1356a68069c0dd2898b7
Matheus Carvalho <113134149+mattcarv@users.noreply.github.com>
2023-10-31 15:52:16 -0400

    Update SGA_analysis.py

--- SGA_analysis.py
+++ SGA_analysis.py
@@ -5,13 +5,19 @@ from astropy.cosmology import FlatLambdaCDM
 cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
 
 # Read the data from the FITS file
-df = pd.read_csv('/home/mdocarm/Downloads/SGA_xmatch.csv')
-df = df[['SGA_ID_1', 'GALAXY', 'RA_LEDA', 'DEC_LEDA', 'MORPHTYPE', 'PA_LEDA',
-         'D25_LEDA', 'BA_LEDA', 'Z_LEDA', 'SB_D25_LEDA', 'FLUX_G', 'FLUX_R',
-         'FLUX_Z', 'FLUX_W1', 'FLUX_W2', 'FLUX_W3', 'FLUX_W4']]
+df = pd.read_csv('/home/mdocarm/Downloads/subsec_SGA.csv')
+# df = df[['SGA_ID_1', 'GALAXY', 'RA_LEDA', 'DEC_LEDA', 'MORPHTYPE', 'PA_LEDA',
+#          'D25_LEDA', 'BA_LEDA', 'Z_LEDA', 'SB_D25_LEDA', 'FLUX_G', 'FLUX_R',
+#          'FLUX_Z', 'FLUX_W1', 'FLUX_W2', 'FLUX_W3', 'FLUX_W4']]
 
-df = df[df.D25_LEDA>0.25]
+# df = df[df.D25_LEDA > 0.25]
+# df = df[df.Z_LEDA > 0]
+# df = df[df.FLUX_W1 > 0]
+# df = df[df.FLUX_W2 > 0]
+# df = df[df.FLUX_W3 > 0]
+# df = df[df.FLUX_W4 > 0]
 
+# df.to_csv('subsec_SGA.csv')
 # Create a histogram
 hist, bins = np.histogram(df['SB_D25_LEDA'], bins=1000)
 
@@ -22,7 +28,8 @@ total_points = len(df['SB_D25_LEDA'])
 lower_10_percent = int(total_points * 0.9)
 
 # Find the bin edges that correspond to the lower 10% of data
-lower_bin_edges = bins[:-1][np.where(np.cumsum(hist) <= lower_10_percent)[0][-1]]
+lower_bin_edges = bins[:-
+                       1][np.where(np.cumsum(hist) <= lower_10_percent)[0][-1]]
 
 # Select data points in the lower 10% magnitudes
 low_sb = df[df['SB_D25_LEDA'] >= lower_bin_edges]
@@ -49,7 +56,8 @@ low_sb = low_sb[low_sb['D25_LEDA'] < d_cutoff]
 low_sb = low_sb.sample(frac=0.7, replace=True, random_state=1)
 
 plt.scatter(low_sb['SB_D25_LEDA'], low_sb['D25_LEDA'], alpha=0.3)
-plt.ylabel('Major axis diameter at the $25 \; mag \; arcsec^{-2}$ isophote (arcmin)')
+plt.ylabel(
+    'Major axis diameter at the $25 \; mag \; arcsec^{-2}$ isophote (arcmin)')
 plt.xlabel('Mean Surface Brightness (B band, $mag \; arcsec^{-2}$)')
 plt.clf()
 
@@ -61,20 +69,51 @@ plt.clf()
 # sns.heatmap(corr, mask=mask, cmap='RdBu', center=0,
 #             square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
-# Calculating luminosities
+# Assigning a distance column
 
-def LumCalc(f, z):
-    
-    d = cosmo.luminosity_distance(z)*(10^(6))
-    f = f > 0
-    sol_m = 3.24
-    
-    m = 22.5 - (2.5*np.log10(f))
-    abs_m = -5*np.log10(d)+5+m
-    
-    l = 10^((-0.4)*(abs_m-sol_m))
-    
-    return l
-    
-#lum = LumCalc(low_sb['FLUX_W1'], low_sb['Z_LEDA'])
-print(np.log10(cosmo.luminosity_distance(0.073)*(1000000)))
+d = cosmo.luminosity_distance(low_sb['Z_LEDA'])
+print(d)
+
+
+
+# # %%
+# z = [0.01, 0.02, 0.03, 0.04, 0.05]
+# flux = [-100, 101, 102, 103, 104]
+# # %%
+# print(low_sb['FLUX_W1'])
+# # %%
+
+
+# def LumCalc(f, z):
+#     L = []
+
+#     for i in range(len(z)):
+#         print('!')
+#         cosm = (cosmo.luminosity_distance(z[i]).value)
+#         d = cosm*(10**6)
+#         # = f[i] > 0
+#         if f> 0:
+#             print('1')
+#             sol_m = 3.24
+#             m = 22.5 - (2.5*np.log10(f))
+#             abs_m = -5*np.log10(d)+5+m
+#             l = 10**((-0.4)*(abs_m-sol_m))
+#         else:
+#             l = 0
+#             print('2')
+#         L.append(l)
+
+#     return L
+
+
+# #lum = LumCalc(flux, z, L)
+# lum = LumCalc(low_sb['FLUX_W1'], low_sb['Z_LEDA'])
+# print(lum)
+# # %%
+# Fl = low_sb['FLUX_W1']
+# #print(Fl)
+# #print(low_sb['FLUX_W1'])
+# #print(low_sb['Z_LEDA'].shape)
+
+# for i in range(2,25):
+#     print(Fl[i])
